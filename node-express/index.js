8428a077c48ed86c930c14aed34258a8e27475db
AnuranDutta <anuran_201700244@smit.smu.edu.in>
2020-10-02 00:31:23 +0530

    Express Simple Rest

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next(); 
+});
+
+// performing get, put, post, del on '/dishes'
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req,res,next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req,res,next) => {
+    res.statusCode = 403;//code 403 means not supported
+    res.end('PUT opertaion not supperted on /dishes');
+});
+
+app.delete('/dishes', (req,res,next) => {
+    res.end('Deleting all the dishes!');
+});
+
+// performing get, put, post, del on '/dishes/$req.params.dishId$'
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req,res,next) => {
+    res.statusCode = 403;//code 403 means not supported
+    res.end('POST opertaion not supperted on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req,res,next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 
 app.use(express.static(__dirname + '/public'))
 

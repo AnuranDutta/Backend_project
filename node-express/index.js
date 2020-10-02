1baab9b716916f80d69f2c72834d51cd702c61f6
AnuranDutta <anuran_201700244@smit.smu.edu.in>
2020-10-03 01:17:33 +0530

    Assignment 1 completed

--- node-express/index.js
+++ node-express/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const http = require('http');
-const morgan = require('morgan');
+const morgan = require('morgan'); //HTTP request middleware logger for Node.js
 const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,49 +14,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next(); 
-});
-
-// performing get, put, post, del on '/dishes'
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req,res,next) => {
-    res.statusCode = 403;//code 403 means not supported
-    res.end('PUT opertaion not supperted on /dishes');
-});
-
-app.delete('/dishes', (req,res,next) => {
-    res.end('Deleting all the dishes!');
-});
-
-// performing get, put, post, del on '/dishes/$req.params.dishId$'
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;//code 403 means not supported
-    res.end('POST opertaion not supperted on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
+app.use('/dishes', dishRouter); //any request coming to '/dishes' will be handled by dishRouter
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'))
 

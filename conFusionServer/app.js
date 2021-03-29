303b9a6356c9da3ee4acd8f9e90527e1a5fc06ee
AnuranDutta <anuran_201700244@smit.smu.edu.in>
2021-03-30 03:26:07 +0530

    Cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -40,34 +40,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));//We are using signed cookies
 
 function auth(req, res, next) {
-	console.log(req.headers);
+	console.log(req.signedCookies);
 
-	let authHeader = req.headers.authorization;
-
-	if (!authHeader) {
-		let err = new Error('You are not authenticated!');
-
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
-	}
-
-	let auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-	let username = auth[0];
-	let password = auth[1];
-
-	if (username === 'admin' && password === 'password') {
-		next();
-	} else {
-		let err = new Error('You are not authenticated!');
-
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
+	if(!req.signedCookies.user){
+		let authHeader = req.headers.authorization;
+	
+		if (!authHeader) {
+			let err = new Error('You are not authenticated!');
+	
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		}
+	
+		let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+	
+		let username = auth[0];
+		let password = auth[1];
+	
+		if (username === 'admin' && password === 'password') {
+			res.cookie('user', 'admin', { signed: true });
+			next();
+		} 
+		else {
+			let err = new Error('You are not authenticated!');
+	
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		}
+	} 
+	else {
+		if(req.signedCookies.user === 'admin') {
+			next();
+		} 
+		else {
+			let err = new Error('You are not authenticated!');
+	
+			err.status = 401;
+			return next(err);
+		}
 	}
 }
 
